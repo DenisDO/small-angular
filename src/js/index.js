3f614c977dc7f42979276af9b010b70138749fa8
Denis Derkach <podstavka128@gmail.com>
2019-02-12 11:48:05 +0200

    feature/directive, init the method directive

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,17 @@
   const directives = [];
 
   const smallAngular = {
+    directive(attrName, callback) {
+        if (!attrName || !callback) {
+            throw new Error('Required parameter is missed!');
+        }
 
+        if (typeof callback !== 'function') {
+            throw new Error('Callback must be a function!');
+        }
+
+        directives.push({attrName, callback});
+    }
   };
 
   window.smallAngular = smallAngular;

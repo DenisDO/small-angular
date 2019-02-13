969a23d81e77f94c2de121e5ccdb1bac4ef9ec3c
Denis Derkach <podstavka128@gmail.com>
2019-02-13 17:53:18 +0200

    fix/smallAngular: code refactoring: - add check for multiply directives - in COMPILE: use getAttributeNames instead of destruction - change element name for entry point

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,10 @@
         throw new Error('Required parameter is missed!');
       }
 
+      if (attrName in directives) {
+        throw new Error('Multiply directives are not allowed!');
+      }
+
       if (typeof callback !== 'function') {
         throw new Error('Callback must be a function!');
       }
@@ -15,9 +19,9 @@
     },
 
     compile(node) {
-      const { attributes } = node;
+      const attributes = node.getAttributeNames();
 
-      [...attributes].forEach(({ name }) => {
+      attributes.forEach(name => {
         if (name in directives) {
           directives[name](node);
         }
@@ -25,17 +29,17 @@
     },
 
     bootstrap(node) {
-      let element = node;
+      let entryPoint = node;
 
       if (!node) {
-        element = document.querySelector('[ng-app]');
+        entryPoint = document.querySelector('[ng-app]');
       }
 
-      if (!element) {
+      if (!entryPoint) {
         throw new Error('There is no entry point!');
       }
-      this.compile(element);
-      element.querySelectorAll('*').forEach(el => {
+      this.compile(entryPoint);
+      entryPoint.querySelectorAll('*').forEach(el => {
         this.compile(el);
       });
     }
